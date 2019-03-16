1a6485a7950f2b701ce1a85fea1377ce7223fac4
Fionn Fuchs <fionn.fuchs@gmail.com>
2019-03-16 16:53:08 +0100

    Add support for reading files

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a javascript framework for drawing monospace ascii on a canvas. In the future functionality for game development is planned.
 
-### Current capabilities:
+### Current features:
 - Draw single characters on grid
 - Draw boxes of characters on grid
 - Draw horizontal and vertical strings on grid
--- ascii-canvas.js
+++ ascii-canvas.js
@@ -3,9 +3,13 @@ var fsize = 15;
 var canvaswidth = 635;
 var canvasheight = 475;
 
+var testx = 0;
+
+var testobject = ""; // <-- test for file reading
+
 var asciimap = {
-	width: Math.floor(canvaswidth/fsize),
-	height: Math.floor(canvasheight/fsize) - 1,
+	width: Math.floor(canvaswidth/fsize*0.9),
+	height: Math.floor(canvasheight/fsize*1.2) - 1,
 	map: [[]],
 
   put: function(char, x, y) {
@@ -42,7 +46,15 @@ var asciimap = {
 				this.map[[x, y]] = null;
 			}
 		}
-	}
+	},
+
+  clear: function() {
+    for (var x = 0; x <= this.width; x++) {
+			for (var y = 0; y <= this.height; y++) {
+				this.map[[x, y]] = null;
+			}
+		}
+  }
 };
 
 window.onload = function() {
@@ -50,41 +62,86 @@ window.onload = function() {
 	cc = c.getContext('2d');
 
 	init();
-	setInterval(update, 1000/60);
+	setInterval(render, 1000/20);
 };
 
 function init() {
-  asciimap.init();
-}
+  //Now files can be read to display objects
+  readTextFile("ascii-object.txt", function(value) {
+    testobject = value;
+  });
 
-function update() {
-	render();
+  asciimap.init();
 }
 
 function render() {
 	cc.fillStyle = 'black';
 	cc.fillRect(0, 0, c.width, c.height);
 
-  asciimap.put('A', 4, 4);
-  asciimap.put('B', 5, 5);
-  asciimap.put('C', 6, 4);
+  // Draw some letters
+  asciimap.put('A', 4 + testx, 4);
+  asciimap.put('B', 5 + testx, 5);
+  asciimap.put('C', 6 + testx, 4);
 
+  // Move letters
+  testx += 1;
+
+  // Draw horizontal strings
   asciimap.putHString("This is the ASCII canvas!", 0, 0);
   asciimap.putHString("Box:", 10, 2);
+
+  // Draw a box of squares
   asciimap.putRect(String.fromCharCode(parseInt(2588,16)), 10, 4, 5, 5);
 
   asciimap.putVString("Vertical string!", 1, 2);
+
+  // Display object with string source for example from a file
+  drawObject(testobject, 10, 20);
+
   renderMap(asciimap);
+  asciimap.clear();
+}
+
+function drawObject(object, x, y) {
+  var _y = 0;
+  var _x = 0;
+  for(var i = 0; i <= object.length; i++) {
+    if(object.charAt(i) != '\n') {
+      asciimap.put(object.charAt(i), x + _x, y + _y);
+      _x++;
+    } else {
+      _x=0;
+      _y++;
+    }
+  }
 }
 
 function renderMap(map) {
-  cc.font = "15px Courier New monospace";
+  cc.font = "15px Courier New";
   cc.fillStyle = 'white';
   for(var y = 0; y <= map.height; y++) {
     for(var x = 0; x <= map.width; x++) {
       if(map.map[[x, y]] != null) {
-        cc.fillText(map.map[[x, y]], x*fsize + 5 + 2, y*fsize + 10 + 2);
+        cc.fillText(map.map[[x, y]], x*fsize*0.9 + 5 + 3, y*fsize*1.2 + 10 + 3);
       }
     }
   }
+}
+
+function readTextFile(file, done)
+{
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function ()
+  {
+      if(rawFile.readyState === 4)
+      {
+          if(rawFile.status === 200 || rawFile.status == 0)
+          {
+              var allText = rawFile.responseText;
+              done(allText);
+          }
+      }
+  }
+  rawFile.send(null);
 }
