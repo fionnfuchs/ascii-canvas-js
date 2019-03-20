1cf594f472b1ce8ccec28dc5a9429cc4225683ba
Fionn Fuchs <fionn.fuchs@gmail.com>
2019-03-20 09:15:50 +0100

    Added mouse input functionality

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import { renderer } from './lib/ascii-canvas.js';
 import { asciimap } from './lib/ascii-canvas.js';
 import { readTextFile } from './lib/utils.js';
+import { mouse } from './lib/mouse.js';
 
-var x = 0;
 var owl;
+var canvas;
 
 window.onload = function() {
     init();
-	setInterval(draw, 1000/20);
+    setInterval(draw, 1000/20);
+    canvas = document.getElementById('ascii-canvas');
+    mouse.init(canvas);
 };
 
 function init() {
@@ -18,8 +21,8 @@ function init() {
 }
 
 function draw() {
-    x = (x+1)%100;
     renderer.drawObject(owl, 2, 2);
     renderer.drawObject("- Hello, I'm Mr. Owl.\n  This is the ascii-canvas.js library.\n  Have Fun!", 9, 3);
+    renderer.drawObject("  Mouse position:\n  X: " + mouse.getMousePos().x + "\n  Y: " + mouse.getMousePos().y, 9, 7, "#999999");
     renderer.render();
 }
