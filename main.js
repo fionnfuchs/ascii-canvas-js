33e8e30799889a9cc6a5aed6f49b639c08c62f30
Fionn Fuchs <fionn.fuchs@gmail.com>
2019-03-26 10:07:03 +0100

    Fix drawObject function of renderer

--- main.js
+++ main.js
@@ -23,6 +23,6 @@ function init() {
 function draw() {
     renderer.drawObject(owl, 2, 2);
     renderer.drawObject("- Hello, I'm Mr. Owl.\n  This is the ascii-canvas.js library.\n  Have Fun!", 9, 3);
-    renderer.drawObject("  Mouse position:\n  X: " + mouse.getMousePos().x + "\n  Y: " + mouse.getMousePos().y, 9, 7, "#999999", "#222222");
+    renderer.drawObject("  Mouse position:\n  X: " + mouse.getMousePos().x + "\n  Y: " + mouse.getMousePos().y + "\n", 9, 7, "#999999", "#222222");
     renderer.render();
 }
