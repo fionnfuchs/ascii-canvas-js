a446615c1b64ad451ee12613cc1dabc53f47c7da
Fionn Fuchs <fionn.fuchs@gmail.com>
2019-03-26 10:24:15 +0100

    Movement demo and Renderer Fix

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ import { mouse } from './lib/mouse.js';
 var owl;
 var canvas;
 
+var movx = 0;
+
 window.onload = function() {
     init();
     setInterval(draw, 1000/20);
@@ -24,6 +26,9 @@ function draw() {
     renderer.drawObject(owl, 2, 2);
     renderer.drawObject("- Hello, I'm Mr. Owl.\n  This is the ascii-canvas.js library.\n  Have Fun!", 9, 3);
     renderer.drawObject("  Mouse position:\n  X: " + mouse.getMousePos().x + "\n  Y: " + mouse.getMousePos().y + "\n", 9, 7, "#999999", "#222222");
-    renderer.drawObject("Background colors!\n", 11, 11, "#EEEEEE", "#D64161")
+    renderer.drawObject("Background colors!\n", 11, 11, "#EEEEEE", "#D64161");
+    renderer.drawObject("Movement!\n", 11, 13, "#EEEEEE", "#6B5B95");
+    renderer.drawObject("@", 11 + movx, 14, "#EEEEEE", "#6B5B95");
+    movx = (movx+1)%18;
     renderer.render();
 }
