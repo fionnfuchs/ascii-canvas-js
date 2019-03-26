ccb6639c3dd83e14b4ac81537566315e57cab782
Fionn Fuchs <fionn.fuchs@gmail.com>
2019-03-26 10:16:31 +0100

    Background color demo update

--- main.js
+++ main.js
@@ -24,5 +24,6 @@ function draw() {
     renderer.drawObject(owl, 2, 2);
     renderer.drawObject("- Hello, I'm Mr. Owl.\n  This is the ascii-canvas.js library.\n  Have Fun!", 9, 3);
     renderer.drawObject("  Mouse position:\n  X: " + mouse.getMousePos().x + "\n  Y: " + mouse.getMousePos().y + "\n", 9, 7, "#999999", "#222222");
+    renderer.drawObject("Background colors!\n", 11, 11, "#EEEEEE", "#D64161")
     renderer.render();
 }
